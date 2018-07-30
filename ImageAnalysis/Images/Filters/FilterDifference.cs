c313ca16a3e21b07b3ef76c4743832ecc9bcadcd
Louis <louis_foy@yahoo.co.uk>
2018-07-30 18:00:18 +0100

    - Difference filter now works correctly - Added ImageMath class to begin simplifying colour operations

--- ImageAnalysis/Images/Filters/FilterDifference.cs
+++ ImageAnalysis/Images/Filters/FilterDifference.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System;
 
 namespace ImageAnalysis.Images.Filters
 {
@@ -21,7 +22,7 @@ namespace ImageAnalysis.Images.Filters
         /// Square radius of pixels to be compared around each pixel
         /// </summary>
         public int PixelRadius = 1;
-
+        
         /// <summary>
         /// The type of difference to be assigned to each pixel
         /// </summary>
@@ -45,33 +46,43 @@ namespace ImageAnalysis.Images.Filters
                 for (int baseY = 0; baseY < bitmap.Height - PixelRadius; ++baseY)
                 {
                     uint* pixelRow = &pixels[baseY * imageData.Stride / 4];
-                    byte* pixelRs = (byte*)pixelRow, pixelGs = (byte*)&pixelRow[1], pixelBs = (byte*)&pixelRow[2];
+                    byte* pixelBs = (byte*)pixelRow, pixelGs = (byte*)&pixelRow[1], pixelRs = (byte*)&pixelRow[2];
 
                     for (int baseX = 0; baseX < bitmap.Width - PixelRadius; ++baseX)
                     {
                         byte curR = pixelRs[baseX << 2], curG = pixelGs[baseX << 2], curB = pixelBs[baseX << 2];
-                        int rDiff = 0, gDiff = 0, bDiff = 0;
+                        int rMin = 255, gMin = 255, bMin = 255;
+                        int rMax = 0, gMax = 0, bMax = 0;
 
                         // Search the radius around this pixel and track the difference
-                        for (int y = 0; y < PixelRadius; ++y)
+                        for (int y = 0; y <= PixelRadius; ++y)
                         {
-                            byte* subRs = (byte*)&pixelRs[y * imageData.Stride], subGs = &subRs[1], subBs = &subGs[1];
+                            byte* subBs = &pixelBs[(y * imageData.Stride) + (baseX << 2)], subGs = &subBs[1], subRs = &subBs[2];
 
-                            for (int x = 0; x < PixelRadius; ++x)
+                            for (int x = 0; x <= PixelRadius; ++x)
                             {
                                 // Compare red
                                 // TODO is there a cleaner way to do this?
-                                rDiff = curR - subRs[x << 2] > rDiff ? curR - subRs[x << 2] : rDiff;
-                                rDiff = subRs[x << 2] - curR > rDiff ? subRs[x << 2] - curR : rDiff;
-                                gDiff = curG - subGs[x << 2] > gDiff ? curG - subGs[x << 2] : gDiff;
-                                gDiff = subGs[x << 2] - curG > gDiff ? subGs[x << 2] - curG : gDiff;
-                                bDiff = curB - subBs[x << 2] > bDiff ? curB - subBs[x << 2] : bDiff;
-                                bDiff = subBs[x << 2] - curB > bDiff ? subBs[x << 2] - curB : bDiff;
+                                if (subRs[x << 2] < rMin)
+                                    rMin = subRs[x << 2];
+                                if (subGs[x << 2] < gMin)
+                                    gMin = subGs[x << 2];
+                                if (subBs[x << 2] < bMin)
+                                    bMin = subBs[x << 2];
+                                if (subRs[x << 2] > rMax)
+                                    rMax = subRs[x << 2];
+                                if (subGs[x << 2] > gMax)
+                                    gMax = subGs[x << 2];
+                                if (subBs[x << 2] > bMax)
+                                    bMax = subBs[x << 2];
                             }
                         }
 
                         // Write the difference to the output
-                        outputPixels[baseY * outputImageData.Stride / 4 + baseX] = 0xFF000000 | (uint)(rDiff << 16 | gDiff << 8 | bDiff);
+                        outputPixels[baseY * outputImageData.Stride / 4 + baseX] = 0xFF000000 | (uint)((rMax - rMin) << 16 | (gMax - gMin) << 8 | (bMax - bMin));
+                        // or the maximum difference of all colour channels..
+                        byte maxDiff = ImageMath.Max((byte)(rMax - rMin), (byte)(gMax - gMin), (byte)(bMax - bMin));
+                        outputPixels[baseY * outputImageData.Stride / 4 + baseX] = 0xFF000000 | (uint)(maxDiff << 16 | maxDiff << 8 | maxDiff);
                     }
                 }
             }
---
+++ ImageAnalysis/Images/ImageMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAnalysis.Images
+{
+    class ImageMath
+    {
+        /// <summary>
+        /// Returns the maximum value of the three byte colour parameters
+        /// </summary>
+        /// <returns>The maximum value of the parameters</returns>
+        public static byte Max(byte r, byte g, byte b)
+        {
+            return (r > g ? (r > b ? r : b) : (g > b ? g : b));
+        }
+    }
+}
